131b1a8b99de09c5951f23f2539ca350614d830f
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2024-02-23 18:34:56 -0500

    416-Creando la seccion de Descuento

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,10 +1,19 @@
 import { Testimonial } from '../models/Testimoniales.js';
 import {Viaje} from '../models/Viaje.js';
 
-const paginaInicio = (req, res) => { // req - lo que enviamos : res - lo que express nos responde
-    res.render('inicio', {
-        pagina: "Inicio",
-    });
+const paginaInicio = async (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    // Consultar 3 viajes del modelo Viaje
+    try {
+        const viajes = await Viaje.findAll({limit: 3})
+        
+        res.render('inicio', {
+            pagina: "Inicio",
+            clase: 'home',
+            viajes
+        });        
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const paginaNosotros = (req, res) => { // req - lo que enviamos : res - lo que express nos responde
