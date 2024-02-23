0b94c1b263b966fae9d78eb5f49060e14e852719
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2024-02-23 15:07:36 -0500

    412-Almacenar los datos en la base de datos

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,4 +1,6 @@
-const guardarTestimonial = (req, res) =>{
+import { Testimonial } from "../models/Testimoniales.js";
+
+const guardarTestimonial = async (req, res) =>{
     // Validar
     const {nombre, correo, mensaje} = req.body;
 
@@ -14,7 +16,28 @@ const guardarTestimonial = (req, res) =>{
         errores.push({mensaje: "Mensaje requerido"});        
     }
 
-    console.log(errores);
+    if (errores.length > 0) {
+        // Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+        })
+    } else {
+        // Almacenarlo en la base de datos
+        try {
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+            res.redirect('/testimoniales');
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 export {guardarTestimonial};
